01769a73163a966c33d636f5ea0ff351149c1ee2
varaya2000 <varaya_2000@yahoo.com>
2025-03-15 22:23:13 -0300

    Implementa una función para agregar amigos, Capturar el valor del campo de entrada:

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+function agregarAmigo(){
+    let amigo = '';
+    amigo = document.getElementById('amigo').value;
+ //   alert(amigo);
+}
